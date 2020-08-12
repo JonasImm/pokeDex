a39891f3b82e80382d26a71650090b4d2ef55370
JonasImm <jonas.imm@gmail.com>
2020-08-12 12:19:30 +0200

    Add basic style

--- src/App.js
+++ src/App.js
@@ -2,7 +2,20 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div>Pokedex</div>;
+  return (
+    <div className="app">
+      <header className="app__header">
+        Pokedex{" "}
+        <input
+          className="pokedex__searchBar"
+          type="text"
+          placeholder="Search"
+        />
+      </header>
+      <main className="pokedex__main">Pokemons</main>
+      <footer className="app__footer">Navigation</footer>
+    </div>
+  );
 }
 
 export default App;
