373871f7ca389c452fa36e15db1e71b78ca94752
JonasImm <jonas.imm@gmail.com>
2020-08-13 13:38:49 +0200

    Create pokeindex automaticly and write basic API function

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import ListItem from "./components/listItem";
 import ListItemIcon from "./components/listItemIcon";
 import ListItemText from "./components/listItemText";
 import BulbasaurSrc from "./assets/bulbasaur.png";
+import IvasaurSrc from "./assets/ivasaur.png";
 import Input from "./components/input";
 import MenuItemPokeMoves from "./components/menuItemPokeMoves";
 import MenuItemPokemon from "./components/menuItemPokemon";
@@ -12,19 +13,31 @@ import MenuItemPokeItems from "./components/menuItemPokeItems";
 import Menu from "./components/menu";
 
 function App() {
+  const [pokemons, setPokemons] = React.useState([]);
+
   return (
     <div className="app">
       <header className="app__header">
         <h1>Pokedex</h1>
         <Input />
+        <button onClick={() => setPokemons("API")}>Get Pokemon</button>
       </header>
       <main className="pokedex__main">
         <List>
-          <ListItem href="#">
-            <ListItemIcon src={BulbasaurSrc} alt="BulbasaurSrc" />
-            <ListItemText primary="Bulbasaur" secondary="#001" />
-            <ListItemIcon src={BulbasaurSrc} alt="BulbasaurSrc" />
-          </ListItem>
+          {pokemons.map((pokemon) => {
+            return (
+              <ListItem href={pokemon.href} key={pokemon.id}>
+                <ListItemIcon
+                  src={pokemon.imgSrc}
+                  alt={`Picture of ${pokemon.name}`}
+                />
+                <ListItemText
+                  primary={pokemon.name}
+                  secondary={`#${pokemon.id}`}
+                />
+              </ListItem>
+            );
+          })}
         </List>
       </main>
       <footer className="app__footer">
---
+++ src/api/pokeApi.js
@@ -0,0 +1,17 @@
+async function fetchPokemon() {
+  const response = await fetch(
+    "https://unpkg.com/pokemons@1.1.0/pokemons.json"
+  );
+  if (!response.ok) {
+    throw new Error(response);
+  }
+  const result = await response.json();
+  const pokemons = result.results.map((pokemon) => ({
+    name: pokemon.name,
+    id: pokemon.national_number,
+    imgSrc: pokemon.sprites.large,
+    href: `#${pokemon.name}`,
+  }));
+
+  return pokemons;
+}
