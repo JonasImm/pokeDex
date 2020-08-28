8b332a47aa4bd56c37c1f889186d7f5d34f02f39
JonasImm <jonas.imm@gmail.com>
2020-08-28 11:19:54 +0200

    Add mongo database

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const app = express();
+const { MongoClient } = require("mongodb");
 
 const port = 3000;
+
+const client = new MongoClient(process.env.MONGO_URL, {
+  useUnifiedTopology: true,
+});
+
+app.get("/", (request, response) => {
+  response.send("Boom. Läuft");
+});
 
 app.listen(port, function () {
   console.log(`Nice. App is listening on http//:localhost:/${port}`);
