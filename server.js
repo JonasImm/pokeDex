ab18bd446e438b322e59b66efb67f245fab0b172
JonasImm <jonas.imm@gmail.com>
2020-08-28 16:38:49 +0200

    Post pokemons to database

--- server.js
+++ server.js
@@ -15,9 +15,16 @@ const client = new MongoClient(process.env.MONGO_URL, {
 async function main() {
   await client.connect();
   const database = client.db(process.env.MONGO_DB_NAME);
-
+  const collection = database.collection("pokemons");
   const pokemons = await fetchPokemons();
   console.log(pokemons);
+
+  app.post("/api/pokemons", async (request, response) => {
+    console.log("post it");
+
+    //const { name, imgSrc, id } = request.body;
+    await collection.insertMany(pokemons);
+  });
 
   app.get("/", (request, response) => {
     response.send("Boom. Läuft");
